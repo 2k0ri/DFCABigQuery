250452069e70456520aaa1f9b84029a02b09d304
2k0ri <esc13245@gmail.com>
2016-02-04 10:03:02 +0900

    initial commit
    copy from https://gitlab.com/aucfan/DFCABigQuery.git

---
+++ DFCABigQuery/MyDotNetActivity.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Globalization;
+using System.Diagnostics;
+using System.Linq;
+
+using Microsoft.Azure.Management.DataFactories.Models;
+using Microsoft.Azure.Management.DataFactories.Runtime;
+
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace MyDotNetActivityNS
+{
+    public class MyDotNetActivity : IDotNetActivity
+    {
+        /// <summary>
+        /// Execute method is the only method of IDotNetActivity interface you must implement. 
+        /// In this sample, the method invokes the Calculate method to perform the core logic.  
+        /// </summary>
+
+        public IDictionary<string, string> Execute(
+            IEnumerable<LinkedService> linkedServices,
+            IEnumerable<Dataset> datasets,
+            Activity activity,
+            IActivityLogger logger)
+        {
+            // to get extended properties (for example: SliceStart)
+            DotNetActivity dotNetActivity = (DotNetActivity)activity.TypeProperties;
+            string sliceStartString = dotNetActivity.ExtendedProperties["SliceStart"];
+
+            // to log all extended properties           
+            IDictionary<string, string> extendedProperties = dotNetActivity.ExtendedProperties;
+            logger.Write("Logging extended properties if any...");
+            foreach (KeyValuePair<string, string> entry in extendedProperties)
+            {
+                logger.Write("<key:{0}> <value:{1}>", entry.Key, entry.Value);
+            }
+
+            // declare types for input and output data stores
+            AzureStorageLinkedService inputLinkedService;
+
+            // declare dataset types
+            CustomDataset inputLocation;
+            AzureBlobDataset outputLocation;
+
+            Dataset inputDataset = datasets.Single(dataset => dataset.Name == activity.Inputs.Single().Name);
+            inputLocation = inputDataset.Properties.TypeProperties as CustomDataset;
+
+            foreach (LinkedService ls in linkedServices)
+                logger.Write("linkedService.Name {0}", ls.Name);
+
+            // using First method instead of Single since we are using the same 
+            // Azure Storage linked service for input and output. 
+            inputLinkedService = linkedServices.First(
+                linkedService =>
+                linkedService.Name ==
+                inputDataset.Properties.LinkedServiceName).Properties.TypeProperties
+                as AzureStorageLinkedService;
+
+            string connectionString = inputLinkedService.ConnectionString; // To create an input storage client.
+            string folderPath = GetFolderPath(inputDataset);
+            string output = string.Empty; // for use later.
+
+            // create storage client for input. Pass the connection string.
+            CloudStorageAccount inputStorageAccount = CloudStorageAccount.Parse(connectionString);
+            CloudBlobClient inputClient = inputStorageAccount.CreateCloudBlobClient();
+
+            // initialize the continuation token before using it in the do-while loop.
+            BlobContinuationToken continuationToken = null;
+            do
+            {   // get the list of input blobs from the input storage client object.
+                BlobResultSegment blobList = inputClient.ListBlobsSegmented(folderPath,
+                                         true,
+                                         BlobListingDetails.Metadata,
+                                         null,
+                                         continuationToken,
+                                         null,
+                                         null);
+
+                // Calculate method returns the number of occurrences of 
+                // the search term (“Microsoft”) in each blob associated
+                // with the data slice. 
+                // 
+                // definition of the method is shown in the next step.
+
+                output = Calculate(blobList, logger, folderPath, ref continuationToken, "Microsoft");
+
+            } while (continuationToken != null);
+
+            // get the output dataset using the name of the dataset matched to a name in the Activity output collection.
+            Dataset outputDataset = datasets.Single(dataset => dataset.Name == activity.Outputs.Single().Name);
+            // convert to blob location object.
+            outputLocation = outputDataset.Properties.TypeProperties as AzureBlobDataset;
+
+            folderPath = GetFolderPath(outputDataset);
+
+            logger.Write("Writing blob to the folder: {0}", folderPath);
+
+            // create a storage object for the output blob.
+            CloudStorageAccount outputStorageAccount = CloudStorageAccount.Parse(connectionString);
+            // write the name of the file. 
+            Uri outputBlobUri = new Uri(outputStorageAccount.BlobEndpoint, folderPath + "/" + GetFileName(outputDataset));
+
+            logger.Write("output blob URI: {0}", outputBlobUri.ToString());
+            // create a new blob and upload the output text.
+            CloudBlockBlob outputBlob = new CloudBlockBlob(outputBlobUri, outputStorageAccount.Credentials);
+            logger.Write("Writing {0} to the output blob", output);
+            outputBlob.UploadText(output);
+
+            // return a new Dictionary object (unused in this code).
+            return new Dictionary<string, string>();
+        }
+
+        /// <summary>
+        /// Gets the folderPath value from the input/output dataset.   
+        /// </summary>
+
+        private static string GetFolderPath(Dataset dataArtifact)
+        {
+            if (dataArtifact == null || dataArtifact.Properties == null)
+            {
+                return null;
+            }
+
+            AzureBlobDataset blobDataset = dataArtifact.Properties.TypeProperties as AzureBlobDataset;
+            if (blobDataset == null)
+            {
+                return null;
+            }
+
+            return blobDataset.FolderPath;
+        }
+
+        /// <summary>
+        /// Gets the fileName value from the input/output dataset.   
+        /// </summary>
+
+        private static string GetFileName(Dataset dataArtifact)
+        {
+            if (dataArtifact == null || dataArtifact.Properties == null)
+            {
+                return null;
+            }
+
+            AzureBlobDataset blobDataset = dataArtifact.Properties.TypeProperties as AzureBlobDataset;
+            if (blobDataset == null)
+            {
+                return null;
+            }
+
+            return blobDataset.FileName;
+        }
+
+        /// <summary>
+        /// Iterates through each blob (file) in the folder, counts the number of instances of search term in the file, 
+        /// and prepares the output text that will be written to the output blob. 
+        /// </summary>
+
+        public static string Calculate(BlobResultSegment Bresult, IActivityLogger logger, string folderPath, ref BlobContinuationToken token, string searchTerm)
+        {
+            string output = string.Empty;
+            logger.Write("number of blobs found: {0}", Bresult.Results.Count<IListBlobItem>());
+            foreach (IListBlobItem listBlobItem in Bresult.Results)
+            {
+                CloudBlockBlob inputBlob = listBlobItem as CloudBlockBlob;
+                if ((inputBlob != null) && (inputBlob.Name.IndexOf("$$$.$$$") == -1))
+                {
+                    string blobText = inputBlob.DownloadText(Encoding.ASCII, null, null, null);
+                    logger.Write("input blob text: {0}", blobText);
+                    string[] source = blobText.Split(new char[] { '.', '?', '!', ' ', ';', ':', ',' }, StringSplitOptions.RemoveEmptyEntries);
+                    var matchQuery = from word in source
+                                     where word.ToLowerInvariant() == searchTerm.ToLowerInvariant()
+                                     select word;
+                    int wordCount = matchQuery.Count();
+                    output += string.Format("{0} occurrences(s) of the search term \"{1}\" were found in the file {2}.\r\n", wordCount, searchTerm, inputBlob.Name);
+                }
+            }
+            return output;
+        }
+    }
+}
